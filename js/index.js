be81187a8404aa29aad2df1ab6509f911d688ea3
lillian417 <49615061+lillian417@users.noreply.github.com>
2020-04-16 14:08:12 +0800

    lijun 0416
    lijun 0416

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(".box1font").hover(function(){
+    $(".box1font img").fadeOut();
+  });
+  $(".box1font").mouseleave(function(){
+    $(".box1font img").fadeIn();
+  });
+  $(".box2font").hover(function(){
+    $(".box2font img").fadeOut();
+  });
+  $(".box2font").mouseleave(function(){
+    $(".box2font img").fadeIn();
+  });
+  $(".box3font").hover(function(){
+    $(".box3font img").fadeOut();
+  });
+  $(".box3font").mouseleave(function(){
+    $(".box3font img").fadeIn();
+  });
+  $(".box4font").hover(function(){
+    $(".box4font img").fadeOut();
+  });
+  $(".box4font").mouseleave(function(){
+    $(".box4font img").fadeIn();
+  });
